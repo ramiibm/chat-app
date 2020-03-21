26812c13837c202c1558e42d356f47fd4bc2a298
ramiibm <rami.ben-maachia@outlook.fr>
2020-03-21 21:34:07 +0100

    add users tracking

--- public/js/chat.js
+++ public/js/chat.js
@@ -72,4 +72,9 @@ $sendLocationButton
         })
     })
 
-socket.emit('join', { username, room })
+socket.emit('join', { username, room }, error => {
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
