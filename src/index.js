97336c3e1edf4cd0e7bf13d9f4c479d7b2f2edfd
ramiibm <rami.ben-maachia@outlook.fr>
2020-03-21 22:49:22 +0100

    send messages to room

--- src/index.js
+++ src/index.js
@@ -27,25 +27,29 @@ io.on('connection', socket => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.emit('message', generateMessage('admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('admin', `${user.username} has joined!`))
 
         callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+
         const filter = new Filter()
 
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
 
-        io.to('Tokyo').emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
     socket.on('sendLocation', (location, callback) => {
-        io.to('Tokyo').emit('locationMessage', generateLocationMessage(location))
+        const user = getUser(socket.id)
+
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, location))
         callback()
     })
 
@@ -53,7 +57,7 @@ io.on('connection', socket => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('admin', `${user.username} has left!`))
         }
     })
 })
