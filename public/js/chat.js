78ee7d5c04405e9cee25003bc9841561d42d0e2b
ramiibm <rami.ben-maachia@outlook.fr>
2020-03-21 18:00:50 +0100

    add location sharing feature - improve ui - events acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,52 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message', message => {
     console.log(message)
 })
 
-document
-    .querySelector('#message-form')
+$messageForm
     .addEventListener('submit', e => {
         e.preventDefault()
+
+        $messageFormButton.setAttribute('disabled', 'disabled')
+
         const message = e.target.elements.message.value
 
-        socket.emit('sendMessage', message)    
+        socket.emit('sendMessage', message, error => {
+            $messageFormButton.removeAttribute('disabled')
+            $messageFormInput.value = ''
+            $messageFormInput.focus()
+
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log('Message delivered!')
+        })
+    })
+
+$sendLocationButton
+    .addEventListener('click', () => {
+        if (!navigator.geolocation) {
+            return alert('Geolocation is not supported by your browser.')
+        }
+
+        $sendLocationButton.setAttribute('disabled', 'disabled')
+
+        navigator.geolocation.getCurrentPosition(position => {
+            const userLocation = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }
+            socket.emit('sendLocation', userLocation, () => {
+                console.log('Location shared!')
+                $sendLocationButton.removeAttribute('disabled')
+            })
+        })
     })
